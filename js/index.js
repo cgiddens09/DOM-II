de510d67a75eb9f1c8129ae012b7229cf6a498de
Christina Giddens <giddens.christina@gmail.com>
2020-02-18 16:55:39 -0500

    completed assignment

--- js/index.js
+++ js/index.js
@@ -1,1 +1,70 @@
-// Your code goes here
+// LOGO--MOUSEENTER EVENT
+const logo = document.querySelector('h1');
+logo.addEventListener('mouseenter', (event) => {
+    logo.style.color = 'blue';
+    logo.style.transform = 'scale(1.2)'
+   event.stopPropagation();
+});
+
+logo.addEventListener('mouseleave', () => {
+    logo.style.color = 'black';
+    logo.style.transform = 'scale(1.0)';
+})
+
+const logo2 = document.querySelector('.container');
+logo2.addEventListener('mouseover', () => {
+    logo2.style.backgroundColor = 'pink';
+    
+})
+
+
+//navigation links rotate
+document.querySelectorAll('.nav-link').forEach(el => {
+    el.addEventListener('click', () => {
+        el.style.transform = 'rotate(7deg)';
+});
+})
+
+//preventDefault
+
+const stopLink = document.querySelector('.nav-link');
+stopLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log('stopped the link');
+})
+
+//page load
+window.addEventListener('load', e => {
+    alert('Page Loaded Successfully');
+})
+
+// bus image
+const bus = document.querySelector('.intro img');
+bus.addEventListener('mousedown', () => {
+    bus.style.transform = 'scale(1.3)';
+})
+bus.addEventListener('mouseup', () => {
+    bus.style.transform = 'scale(1.0)';
+})
+
+//body color
+const bod = document.querySelector('body');
+bod.addEventListener('dblclick', e => {
+    bod.style.backgroundColor = 'rebeccapurple';
+})
+
+//orientation change
+window.addEventListener('orientationchange', () => {
+    alert('You changed the orientation');
+})
+
+//copy
+const copier = document.querySelector('.text-content');
+copier.addEventListener('copy', () => {
+    copier.style.color = 'red';
+})
+
+
+window.addEventListener('resize', () => {
+    alert('put me back!');
+})
